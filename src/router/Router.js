3d5f705d29ed1eb3c93761a55f8545b1e33a75f2
habbkad <habibkadiri52@gmail.com>
2023-07-10 15:53:53 +0000

    Done with router setup

---
+++ src/router/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/home_page/Home";
+import Form from "../pages/form_page/Form";
+import NavBar from "../components/navbar/Navbar";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
