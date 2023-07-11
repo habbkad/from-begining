8460934b4a98d0db27beb14771b016211f9551c3
Derek Atiawu <derekatiawu@gmail.com>
2023-07-11 05:23:24 +0000

    Did some header styling

--- src/pages/home_page/Home.js
+++ src/pages/home_page/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+// import "./pages/home_page/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>This is the home page</h1>
+    </div>
+  );
 }
 
 export default Home;
